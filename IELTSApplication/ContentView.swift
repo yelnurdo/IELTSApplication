159f0e23d91294d566403db65c7a1524df661729
yelnurdo <yelnurdos@gmail.com>
2024-01-25 19:07:17 +0600

    Initial Commit

---
+++ IELTSApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IELTSApplication
+//
+//  Created by Yelnur Dossymov on 25.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IELTSApplication/IELTSApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IELTSApplicationApp.swift
+//  IELTSApplication
+//
+//  Created by Yelnur Dossymov on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IELTSApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
